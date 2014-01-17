49f0e7368a0e77e041762e71c5b7beba46bc2e26
Conrad Irwin <conrad.irwin@gmail.com>
2014-01-17 10:55:42 -0800

    Better comments

--- lib/mongoid-rails.rb
+++ lib/mongoid-rails.rb
@@ -2,5 +2,8 @@ require 'mongoid'
 require 'strong_parameters'
 require 'mongoid-rails/forbidden_query_protection'
 
+# From https://github.com/rails/strong_parameters/issues/32
 Mongoid::Document.send(:include, ActiveModel::ForbiddenAttributesProtection)
+
+# From https://github.com/mongoid/mongoid/commit/f02144f3af7f798187ec2133dfb615c973334ffe
 Mongoid::Criteria.send(:include, MongoidRails::ForbiddenQueryProtection)
